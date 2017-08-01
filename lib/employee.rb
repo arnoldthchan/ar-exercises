401b96d693950d18c08e2f04441911435adf072e
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 20:51:24 +0000

    Split up methods for a before/after creation

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,15 +3,20 @@ class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates_inclusion_of :hourly_rate, in: 40..200
   validates_inclusion_of :store_id, in: 1..2
-  # before_create :testing
-  after_create :testing # Will not be able to set the password
+  # before_create :assign_before
+  after_create :assign_after # Will not be able to set the password
 
 
   private
-    def testing
+    def assign_before
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
       self.attributes = {password: generated_password}
     end
 
+    def assign_after
+      generated_password = (0...8).map { (65 + rand(26)).chr }.join
+      puts "Generating password: #{generated_password}"
+      self.attributes = {password: generated_password}
+    end
 end
