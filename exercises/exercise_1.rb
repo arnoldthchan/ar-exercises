6cd6d185267dc83b8c6664a8f77e711726ab1618
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 16:58:00 +0000

    changed p to puts

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,4 +7,4 @@ Store.create(name:"Burnaby", annual_revenue:300000, mens_apparel:true, womens_ap
 Store.create(name:"Richmond", annual_revenue:1260000, mens_apparel:false, womens_apparel:true)
 Store.create(name:"Gastown", annual_revenue:190000, mens_apparel:true, womens_apparel:false)
 
-pp Store.count
+puts Store.count
