96a0ecee375d40accf748a1e6e7127ce509724d8
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 17:33:00 +0000

    Completed exercise 6 and added relationship between store and employee tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ted", last_name: "Tederson", hourly_rate: 12)
+@store1.employees.create(first_name: "Bob", last_name: "Boberson", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Jack", last_name: "Jackson", hourly_rate: 50)
+@store2.employees.create(first_name: "Mick", last_name: "Mickson", hourly_rate: 14)
