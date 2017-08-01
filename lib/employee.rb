3aec7afaab49de52d8dca0b5eb4bed4074e7c112
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 20:54:04 +0000

    Changed method name for clarity

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,18 +3,18 @@ class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates_inclusion_of :hourly_rate, in: 40..200
   validates_inclusion_of :store_id, in: 1..2
-  before_create :assign_before
-  # after_create :assign_after
+  before_create :assign_password_before
+  # after_create :assign_password_after
 
 
   private
-    def assign_before
+    def assign_password_before
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
       self.attributes = {password: generated_password}
     end
 
-    def assign_after
+    def assign_password_after
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
       self.update = (password: generated_password)
