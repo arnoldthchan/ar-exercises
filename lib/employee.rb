114900b78a7bc4b881dd689d0a66f1c72d79765f
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 20:53:29 +0000

    fixed after_create password assignment method

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,8 +3,8 @@ class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates_inclusion_of :hourly_rate, in: 40..200
   validates_inclusion_of :store_id, in: 1..2
-  # before_create :assign_before
-  after_create :assign_after # Will not be able to set the password
+  before_create :assign_before
+  # after_create :assign_after
 
 
   private
@@ -17,6 +17,6 @@ class Employee < ActiveRecord::Base
     def assign_after
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
-      self.attributes = {password: generated_password}
+      self.update = (password: generated_password)
     end
 end
