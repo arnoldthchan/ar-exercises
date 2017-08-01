8f808d2621a11b3e3147e93628711890528e339d
Arnold Chan <arnoldthchan@gmail.com>
2017-08-01 22:01:10 +0000

    removed one of the methods

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,27 +3,22 @@ class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates_inclusion_of :hourly_rate, in: 40..200
   validates_inclusion_of :store_id, in: 1..2
-  # before_create :assign_password_before_create
+  # before_create :assign_password_before
   # after_create :assign_password_after_create
-  before_save :assign_password_before_save #Runs after create callbacks
+  before_save :assign_password_before #Runs after create callbacks
 
 
   private
     def assign_password_before_create
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
-      self.attributes = {password: generated_password}
+      self.attributes = {password: generated_password} unless !self.password.blank?
     end
 
     def assign_password_after_create
       generated_password = (0...8).map { (65 + rand(26)).chr }.join
       puts "Generating password: #{generated_password}"
-      self.update(password: generated_password)
+      self.update(password: generated_password) unless !self.password.blank?
     end
 
-    def assign_password_before_save
-      generated_password = (0...8).map { (65 + rand(26)).chr }.join
-      puts "Generating password: #{generated_password}"
-      self.attributes = {password: generated_password}
-    end
 end
